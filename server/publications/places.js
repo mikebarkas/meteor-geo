fc7187a865fcfb0b33dc5ccd71dad32c4ed37e70
Mike Barkas <mike@barkas.com>
2015-09-23 22:29:39 -0400

    Publish list.

--- server/publications/places.js
+++ server/publications/places.js
@@ -1,13 +1,7 @@
 Meteor.publish('places', function () {
-  return Places.find({}, {sort: {pop:-1}, limit:100});
+  return Places.find({}, {sort: {pop:-1}, limit:20});
 });
 
 Meteor.publish('cityList', function () {
-  var self = this;
-  var cityList = Places.aggregate([
-    {'$group' : {'_id': '$city'}}
-  ])
-  _(cityList).each(function() {
-    self.added('cityList', Random.id(), {city: 'city test'});
-  });
+  return Places.find({}, {fields: {city: 1, state: 1}, limit:80});
 });
